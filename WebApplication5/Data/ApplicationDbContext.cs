269b2d9430095d83de4850f3df3b8279bc32c420
taichan03 <tai.c.chan3@gmail.com>
2022-09-30 06:02:47 -0400

    Added Database Tables and Migrations, End Implement Database Section

--- WebApplication5/Data/ApplicationDbContext.cs
+++ WebApplication5/Data/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LeaveManagement.Web.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication5.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+
+
+
     }
 }
---
+++ WebApplication5/Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveType : BaseEntity
+    {
+
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+
+
+
+    }
+}
