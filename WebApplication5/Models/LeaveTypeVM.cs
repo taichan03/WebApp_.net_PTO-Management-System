d8c3ac83b7d58dd3971b143059abeb4dd313b09c
taichan03 <tai.c.chan3@gmail.com>
2022-10-03 05:48:19 -0400

    Updated the Create page for form

--- WebApplication5/Models/LeaveTypeVM.cs
+++ WebApplication5/Models/LeaveTypeVM.cs
@@ -6,9 +6,12 @@ namespace LeaveManagement.Web.Models
     public class LeaveTypeVM
     {
         public int Id { get; set; }
+        [Display(Name = "Leave Type Name")]
+        [Required]
         public string Name { get; set; }
 
         [Display(Name = "Default Number Of Days")]
+        [Required]
         public int DefaultDays { get; set; }
 
 
