4456a52d403b96927c4433e9ea6583c94014a76a
taichan03 <tai.c.chan3@gmail.com>
2022-10-18 21:34:11 -0400

    Modified the crud screens and added datatables and swal. End of Leave Type Management Module

--- WebApplication5/Configurations/MapperConfig.cs
+++ WebApplication5/Configurations/MapperConfig.cs
@@ -5,9 +5,7 @@ using LeaveManagement.Web.Models;
 namespace LeaveManagement.Web.Configurations
 {
     public class MapperConfig : Profile
-
-    {
-
+    { 
         public MapperConfig()
         {
             CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
