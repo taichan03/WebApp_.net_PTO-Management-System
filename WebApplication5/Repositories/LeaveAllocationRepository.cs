5b871cef5d7df86f126a910117cb77cf81dd48cc
taichan03 <tai.c.chan3@gmail.com>
2023-02-18 22:07:06 -0500

    Added more stuff to leave allocation.

--- WebApplication5/Repositories/LeaveAllocationRepository.cs
+++ WebApplication5/Repositories/LeaveAllocationRepository.cs
@@ -9,14 +9,35 @@ namespace LeaveManagement.Web.Repositories
     public class LeaveAllocationRepository : GenericRepostiory<LeaveAllocation>, ILeaveAllocationRepository
     {
         private readonly UserManager<Employee> userManager;
-        public LeaveAllocationRepository(ApplicationDbContext context, UserManager<Employee> userManager) : base(context)
+        private readonly ILeaveTypeRepository leaveTypeRepository;
+
+        public LeaveAllocationRepository(ApplicationDbContext context, 
+            UserManager<Employee> userManager, ILeaveTypeRepository leaveTypeRepository) : base(context)
         {
             this.userManager = userManager;
+            this.leaveTypeRepository = leaveTypeRepository;
         }
 
         public async Task LeaveAllocation(int leaveTypeId)
         {
             var employees = await userManager.GetUsersInRoleAsync(Roles.User);
+            var period = DateTime.Now.Year;
+            var leaveType = await leaveTypeRepository.GetAsync(leaveTypeId);
+
+            foreach(var employee in employees) 
+            {
+                var allocation = new LeaveAllocation
+                {
+                    EmployeeId = employee.Id,
+                    LeaveTypeId= leaveTypeId,
+                    Period = period,
+                    NumberOfDays = leaveType.DefaultDays
+                };
+                await AddAsync(allocation);
+                
+
+            }
+
 
             throw new NotImplementedException();
         }
