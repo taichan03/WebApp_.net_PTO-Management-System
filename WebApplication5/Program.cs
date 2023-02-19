75e648306d99ed6df7a9aa5496bbb51ccb192101
taichan03 <tai.c.chan3@gmail.com>
2023-02-19 09:07:36 -0500

    Update the leave allocation.

--- WebApplication5/Program.cs
+++ WebApplication5/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddTransient<IEmailSender>(s => new EmailSender("localhost", 25
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepostiory<>));
 builder.Services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+builder.Services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
 
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 
