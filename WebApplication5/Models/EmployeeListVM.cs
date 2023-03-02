6db39f4f38fb8fa346bf6916b8165850805566b5
taichan03 <tai.c.chan3@gmail.com>
2023-03-02 18:01:08 -0500

    Added email addresses

--- WebApplication5/Models/EmployeeListVM.cs
+++ WebApplication5/Models/EmployeeListVM.cs
@@ -1,12 +1,23 @@
-﻿namespace LeaveManagement.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
 {
     public class EmployeeListVM
     {
 
         public string Id { get; set; }
+
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Date Joined")]
         public string DateJoined { get; set; }
+
+                [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
 
 
 
