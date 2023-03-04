fb4e21dbf7a765930bd99949bc3ba97b17941654
taichan03 <tai.c.chan3@gmail.com>
2023-03-04 11:01:17 -0500

    Added ViewAllocations view.

--- WebApplication5/Models/LeaveAllocationVM.cs
+++ WebApplication5/Models/LeaveAllocationVM.cs
@@ -6,7 +6,7 @@ namespace LeaveManagement.Web.Models
 {
     public class LeaveAllocationVM
     {
-
+        public int Id { get; set; }
         public int NumberOfDays { get; set; }
         public int Period { get; set; }
         public LeaveTypeVM LeaveType { get; set; }
