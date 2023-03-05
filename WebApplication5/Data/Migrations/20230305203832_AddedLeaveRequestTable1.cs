bff52eee918f8e16f5889235e2e39289fc9a5fa1
taichan03 <tai.c.chan3@gmail.com>
2023-03-05 15:54:19 -0500

    Leave Request Contraints.

---
+++ WebApplication5/Data/Migrations/20230305203832_AddedLeaveRequestTable1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication5.Data.Migrations
+{
+    public partial class AddedLeaveRequestTable1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cac43a6e-f7bb-4448-baaf-1add431ccbaf",
+                column: "ConcurrencyStamp",
+                value: "eafbb247-1667-4a53-97d8-4c977d6d382c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cac43a6e-f7bb-4448-baaf-1add431ccbbf",
+                column: "ConcurrencyStamp",
+                value: "f1ac49cb-2020-4014-a544-5412d253befd");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cac43a6e-f7bb-4448-baaf-1add431ccbaf",
+                column: "ConcurrencyStamp",
+                value: "bdbc056a-40e4-4f02-a4ef-edf5e972b209");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cac43a6e-f7bb-4448-baaf-1add431ccbbf",
+                column: "ConcurrencyStamp",
+                value: "dbfbc375-96fc-44cf-bf82-74d75b3f7a93");
+        }
+    }
+}
