cbbf36583965f5b06aba558bb31e238e39639808
taichan03 <tai.c.chan3@gmail.com>
2023-03-06 21:53:42 -0500

    MyLeave.

--- WebApplication5/Contracts/ILeaveRequestRepository.cs
+++ WebApplication5/Contracts/ILeaveRequestRepository.cs
@@ -6,9 +6,9 @@ namespace LeaveManagement.Web.Contracts
     public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
     {
         Task<bool> CreateLeaveRequest(LeaveRequestCreateVM model);
-        //Task<EmployeeLeaveRequestViewVM> GetMyLeaveDetails();
+        Task<EmployeeLeaveRequestViewVM> GetMyLeaveDetails();
         //Task<LeaveRequestVM> GetLeaveRequestAsync(int? id);
-        //Task<List<LeaveRequestVM>> GetAllAsync(string employeeId);
+        Task<List<LeaveRequestVM>> GetAllAsync(string employeeId);
         Task ChangeApprovalStatus(int leaveRequestId, bool approved);
         Task CancelLeaveRequest(int leaveRequestId);
         //Task<AdminLeaveRequestViewVM> GetAdminLeaveRequestList();
