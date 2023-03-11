90e567718e0849d3697f415ed2398cf0d2bfa596
taichan03 <tai.c.chan3@gmail.com>
2023-03-11 12:00:52 -0500

    GetEmployeeAllocation

--- WebApplication5/Repositories/LeaveAllocationRepository.cs
+++ WebApplication5/Repositories/LeaveAllocationRepository.cs
@@ -133,7 +133,7 @@ namespace LeaveManagement.Application.Repositories
             return true;
         }
 
-        public async Task<LeaveAllocation> GetEmployeeAllocation(string employeeId, int leaveTypeId)
+        public async Task<LeaveAllocation?> GetEmployeeAllocation(string employeeId, int leaveTypeId)
         {
             return await context.LeaveAllocations.FirstOrDefaultAsync(q => q.EmployeeId == employeeId && q.LeaveTypeId == leaveTypeId);
         }
